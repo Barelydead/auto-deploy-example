377ce9a5ad83421841c709370646382fbd1f8f7e
Christofer Jungberg <kiffen_@hotmail.com>
2018-01-24 16:29:07 +0100

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.standard')
+
+@section('title', 'About page')
+
+
+
+@section('content')
+    <p class="text-danger">This is the <strong>about</strong> page.</p>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.standard')
+
+@section('title', 'Page Title')
+
+
+
+@section('content')
+    <p class="text-danger">This is the <strong>home</strong> page.</p>
+@endsection
