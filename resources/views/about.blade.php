a0b071e3351eab7ed3f7756bc8330bd2fa2362e5
Magnus Andersson <bredsjomagnus@yahoo.se>
2018-02-02 14:56:58 +0000

    About - all screen, styling

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -58,115 +58,11 @@
 
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-md-12">
-            <img width='100%' src="https://i.imgur.com/QGAjpqE.png" title="rdc" alt='about rdc' />
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="pillow-50">
-
-        </div>
-    </div>
-
-    <!-- FOUR CHAPTERS FROM BULLET LIST -->
-    <div class="row">
-        <div class="col-md-2 col-md-offset-4">
-            <h3 id='historyinfo' class="bold text-danger">OUR HISTORY</h3>
-        </div>
-    </div>
-    <div class="row rdcseablue rdcsandbordertop">
-        <div class="col-md-5 col-md-offset-4">
-            <div class="pillow-20">
-
-            </div>
-            <p class="lightlightgray">
-                RDC coatings can be easily applied to just about any surface or substrate, including wood, steel, concrete, asphalt, stucco, fiberglass, carbon fiber, glass, plastics/PVC, most composites (you name it) with a standard brush, roller or sprayer like traditional paints, most often without the need of primers. Our coatings are “dry-to-touch” typically in only 15-20 minutes and the second coat can usually be applied within only 1 hour, depending on local weather conditions. Clean-up is fast and easy with simple soap and water.
-            </p>
-            <div class="pillow-20">
-
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="pillow-20">
-
-        </div>
-        <div class="col-md-2 col-md-offset-4">
-            <h3 id='purposeinfo' class="bold text-danger">OUR PURPOSE</h3>
-        </div>
-    </div>
-    <div class="row rdcsandbordertop rdcdeepred">
-        <div class="col-md-5 col-md-offset-4">
-            <div class="pillow-20">
-
-            </div>
-            <p class="lightlightgray">
-                RDC coatings can be easily applied to just about any surface or substrate, including wood, steel, concrete, asphalt, stucco, fiberglass, carbon fiber, glass, plastics/PVC, most composites (you name it) with a standard brush, roller or sprayer like traditional paints, most often without the need of primers. Our coatings are “dry-to-touch” typically in only 15-20 minutes and the second coat can usually be applied within only 1 hour, depending on local weather conditions. Clean-up is fast and easy with simple soap and water.
-            </p>
-            <div class="pillow-20">
-
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="pillow-20">
-
-        </div>
-        <div class="col-md-2 col-md-offset-4">
-            <h3 id='peopleinfo' class="bold text-danger">OUR PEOPLE</h3>
-        </div>
-    </div>
-    <div class="row rdcblack rdcsandbordertop">
-
-        <div class="col-md-5 col-md-offset-4">
-            <div class="pillow-20">
-
-            </div>
-            <p class="lightlightgray">
-                RDC coatings can be easily applied to just about any surface or substrate, including wood, steel, concrete, asphalt, stucco, fiberglass, carbon fiber, glass, plastics/PVC, most composites (you name it) with a standard brush, roller or sprayer like traditional paints, most often without the need of primers. Our coatings are “dry-to-touch” typically in only 15-20 minutes and the second coat can usually be applied within only 1 hour, depending on local weather conditions. Clean-up is fast and easy with simple soap and water.
-            </p>
-            <div class="pillow-20">
-
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="pillow-20">
-
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-12">
-            <center>
-                <img width='80%' src="https://i.imgur.com/WarnQaT.png" title="rdc" alt='about rdc' />
-            </center>
-        </div>
-    </div>
-
-
-    <div class="row">
-        <div class="pillow-50">
-
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="pillow-20">
-
-        </div>
-        <div class="col-md-2 col-md-offset-4">
-            <h3 id='productinfo' class="bold text-danger">THE PRODUCT</h3>
-        </div>
-    </div>
     <div class="row rdcblack">
-        <div class="col-md-5 col-md-offset-4">
+        <div class="col-md-2 col-md-offset-2">
+            <h4 class="rdcred-text bold">THE PRODUCT</h4>
+        </div>
+        <div class="col-md-6">
             <div class="pillow-20">
 
             </div>
