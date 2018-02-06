34b067dc3bab91ab1d2dd6d805d931bcb6b314f8
Magnus Andersson <bredsjomagnus@yahoo.se>
2018-02-06 06:18:49 +0000

    Style on about page. Experimenting with colors and borders

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -24,6 +24,7 @@
             <ul>
                 <li>
                     <a class='rdcred-link' href='#historyinfo'>Our History</a>
+<<<<<<< b20e9b5722a5f9afc73526899a7c830abfb8bb1e
                 </li>
                 <li>
                     <a class='rdcred-link' href='#purposeinfo'>Our Purpose</a>
@@ -32,6 +33,16 @@
                     <a class='rdcred-link' href='#peopleinfo'>Our People</a>
                 </li>
                 <li>
+=======
+                </li>
+                <li>
+                    <a class='rdcred-link' href='#purposeinfo'>Our Purpose</a>
+                </li>
+                <li>
+                    <a class='rdcred-link' href='#peopleinfo'>Our People</a>
+                </li>
+                <li>
+>>>>>>> Style on about page. Experimenting with colors and borders
                     <a class='rdcred-link' href='#productinfo'>The Product</a>
                 </li>
             </ul>
@@ -58,17 +69,124 @@
 
         </div>
     </div>
-    <div class="row rdcblack">
-        <div class="col-md-2 col-md-offset-2">
-            <h4 class="rdcred-text bold">THE PRODUCT</h4>
+
+    <div class="row">
+        <div class="col-md-12">
+            <img width='100%' src="https://i.imgur.com/QGAjpqE.png" title="rdc" alt='about rdc' />
         </div>
-        <div class="col-md-6">
+    </div>
+
+    <div class="row">
+        <div class="pillow-50">
+
+        </div>
+    </div>
+
+    <!-- FOUR CHAPTERS FROM BULLET LIST -->
+    <div class="row">
+        <div class="col-md-2 col-md-offset-4">
+            <h3 id='historyinfo' class="bold text-danger">OUR HISTORY</h3>
+        </div>
+    </div>
+    <div class="row rdcseablue rdcsandbordertop">
+        <div class="col-md-5 col-md-offset-4">
             <div class="pillow-20">
 
             </div>
             <p class="lightlightgray">
                 RDC coatings can be easily applied to just about any surface or substrate, including wood, steel, concrete, asphalt, stucco, fiberglass, carbon fiber, glass, plastics/PVC, most composites (you name it) with a standard brush, roller or sprayer like traditional paints, most often without the need of primers. Our coatings are “dry-to-touch” typically in only 15-20 minutes and the second coat can usually be applied within only 1 hour, depending on local weather conditions. Clean-up is fast and easy with simple soap and water.
             </p>
+            <div class="pillow-20">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="pillow-20">
+
+        </div>
+        <div class="col-md-2 col-md-offset-4">
+            <h3 id='purposeinfo' class="bold text-danger">OUR PURPOSE</h3>
+        </div>
+    </div>
+    <div class="row rdcsandbordertop rdcdeepred">
+        <div class="col-md-5 col-md-offset-4">
+            <div class="pillow-20">
+
+            </div>
+            <p class="lightlightgray">
+                RDC coatings can be easily applied to just about any surface or substrate, including wood, steel, concrete, asphalt, stucco, fiberglass, carbon fiber, glass, plastics/PVC, most composites (you name it) with a standard brush, roller or sprayer like traditional paints, most often without the need of primers. Our coatings are “dry-to-touch” typically in only 15-20 minutes and the second coat can usually be applied within only 1 hour, depending on local weather conditions. Clean-up is fast and easy with simple soap and water.
+            </p>
+            <div class="pillow-20">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="pillow-20">
+
+        </div>
+        <div class="col-md-2 col-md-offset-4">
+            <h3 id='peopleinfo' class="bold text-danger">OUR PEOPLE</h3>
+        </div>
+    </div>
+    <div class="row rdcblack rdcsandbordertop">
+
+        <div class="col-md-5 col-md-offset-4">
+            <div class="pillow-20">
+
+            </div>
+            <p class="lightlightgray">
+                RDC coatings can be easily applied to just about any surface or substrate, including wood, steel, concrete, asphalt, stucco, fiberglass, carbon fiber, glass, plastics/PVC, most composites (you name it) with a standard brush, roller or sprayer like traditional paints, most often without the need of primers. Our coatings are “dry-to-touch” typically in only 15-20 minutes and the second coat can usually be applied within only 1 hour, depending on local weather conditions. Clean-up is fast and easy with simple soap and water.
+            </p>
+            <div class="pillow-20">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="pillow-20">
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <center>
+                <img width='80%' src="https://i.imgur.com/WarnQaT.png" title="rdc" alt='about rdc' />
+            </center>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="pillow-50">
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="pillow-20">
+
+        </div>
+        <div class="col-md-2 col-md-offset-4">
+            <h3 id='productinfo' class="bold text-danger">THE PRODUCT</h3>
+        </div>
+    </div>
+    <div class="row rdcblack">
+        <div class="col-md-5 col-md-offset-4">
+            <div class="pillow-20">
+
+            </div>
+            <p class="lightlightgray">
+                RDC coatings can be easily applied to just about any surface or substrate, including wood, steel, concrete, asphalt, stucco, fiberglass, carbon fiber, glass, plastics/PVC, most composites (you name it) with a standard brush, roller or sprayer like traditional paints, most often without the need of primers. Our coatings are “dry-to-touch” typically in only 15-20 minutes and the second coat can usually be applied within only 1 hour, depending on local weather conditions. Clean-up is fast and easy with simple soap and water.
+            </p>
+            <div class="pillow-20">
+
+            </div>
         </div>
     </div>
 
