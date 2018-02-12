525bd263f9aa54262855a127f038cc86113d2716
Christofer Jungberg <kiffen_@hotmail.com>
2018-02-12 15:31:06 +0100

    adding travis config file to automate build

--- resources/views/performance-test.blade.php
+++ resources/views/performance-test.blade.php
@@ -21,42 +21,68 @@ counting.")
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-md-12">
-                <h1>12+ Year (In-Can) Liquid A2 Product Shelf-Life Test  </h1>
+        <article class="article rdcseablue lightlightgray">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>12+ Year (In-Can) Liquid A2 Product Shelf-Life Test  </h1>
+                    <div class="pillow-20"></div>
+                </div>
             </div>
-        </div>
 
 
-        <div class="row">
-            <div class="col-md-4">
-                <h4>Re-opened After 12 Years.</h4>
-                <p>The two images (RIGHT) depict one of six original 1-quart containers of RDC Formula A2 as they appeared when they were reopened in August 2017, with what appears to be some resin (in good condition) floating on the top. This product was originally manufactured and canned in August 2005—12 years prior. These containers had been opened several times over time and were stored indoors under extreme hot and cold environments, with ambient room temperatures ranging from 18°f-160°f for all of the 12 years. </p>
+            <div class="row">
+                <div class="col-md-4">
+                    <h4>Re-opened After 12 Years.</h4>
+                    <p>The two images (RIGHT) depict one of six original 1-quart containers of RDC Formula A2 as they appeared when they were reopened in August 2017, with what appears to be some resin (in good condition) floating on the top. This product was originally manufactured and canned in August 2005—12 years prior. These containers had been opened several times over time and were stored indoors under extreme hot and cold environments, with ambient room temperatures ranging from 18°f-160°f for all of the 12 years. </p>
+                </div>
+                <div class="col-md-4">
+                    <img src="img/performance/paint.jpg" class="contained-img img-h-300">
+                </div>
+                <div class="col-md-4">
+                    <img src="img/performance/dry_paint.jpg" class="contained-img img-h-300">
+                </div>
             </div>
-            <div class="col-md-4">
-                <img src="img/performance/paint.jpg" class="contained-img-width">
-            </div>
-            <div class="col-md-4">
-                <img src="img/performance/dry_paint.jpg" class="contained-img-width">
-            </div>
-        </div>
 
-        <div class="pillow-30">
+            <div class="pillow-30">
 
-        </div>
+            </div>
 
-        <div class="row">
-            <div class="col-md-4">
-                <img src="img/performance/rehydrated_paint.jpg" class="contained-img-width">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="img/performance/rehydrated_paint.jpg" class="contained-img img-h-300">
+                </div>
+                <div class="col-md-4">
+                    <img src="img/performance/rehydrated_paint2.jpg" class="contained-img img-h-300">
+                </div>
+                <div class="col-md-4">
+                    <h4>After water added and shaken.</h4>
+                    <p>The two images (LEFT) depict (above shown) 12 year old RDC Formula A2 after the product was transferred to a new 1-quart paint can, then a small amount of tap water was added and the can was shaken at a local paint outlet. Amazingly all product remained fully usable after 12 years of storage, all of which was actually used in 2017 to create additional test samples now in progress and shown in this presentation. This is unarguably an AMAZING, unmatched discovery. RDC is not aware of any paint or coating company on this planet that can claim (and prove) a 12+ year shelf-life. </p>
+                </div>
             </div>
-            <div class="col-md-4">
-                <img src="img/performance/rehydrated_paint2.jpg" class="contained-img-width">
+        </article>
+
+        <article class="article rdcred lightlightgray">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>12+ Year Old R33 & R38 Roof Product Sample Tests </h1>
+                    <div class="pillow-20"></div>
+                </div>
             </div>
-            <div class="col-md-4">
-                <h4>After water added and shaken.</h4>
-                <p>The two images (LEFT) depict (above shown) 12 year old RDC Formula A2 after the product was transferred to a new 1-quart paint can, then a small amount of tap water was added and the can was shaken at a local paint outlet. Amazingly all product remained fully usable after 12 years of storage, all of which was actually used in 2017 to create additional test samples now in progress and shown in this presentation. This is unarguably an AMAZING, unmatched discovery. RDC is not aware of any paint or coating company on this planet that can claim (and prove) a 12+ year shelf-life. </p>
+
+
+            <div class="row">
+                <div class="col-md-4">
+                    <h4>Re-opened After 12 Years.</h4>
+                    <p>Images (RIGHT) depict RDC Roof Formulas R33 & R38 coated bare on two 1/8” thick asphalt roof coating samples in 2005.  Both samples experienced NO DELAMINATION,  NO COATING FAILURE whatsoever, even when bent at 180°—after 12+ years.  This discovery is highly remarkable, especially considering the coating remains like new and is fully pliable, even at a 180° bend, over 12 years after it was applied.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="img/performance/roof_slab1.jpg" class="contained-img img-h-200">
+                </div>
+                <div class="col-md-4">
+                    <img src="img/performance/roof_slab1_bend.jpg" class="contained-img img-h-200">
+                </div>
             </div>
-        </div>
+        </article>
     {{-- container close --}}
     </div>
 @endsection
