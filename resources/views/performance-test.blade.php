7c5abc2d551f97433003b9e3fe4b6f368de8494e
Christofer Jungberg <kiffen_@hotmail.com>
2018-02-13 14:27:01 +0100

    improved research page

--- resources/views/performance-test.blade.php
+++ resources/views/performance-test.blade.php
@@ -17,11 +17,11 @@ counting.")
 
         <div class="row">
             <div class="col-md-12">
-                <p class="rdcorange-text">This presentation covers several ongoing, practical application performance tests of RDC’s two signature Advanced, High-Performance, Aerospace Grade, Acrylic, Ceramic Coatings. Primers were NEVER applied on any item tested. In most cases, two coats of RDC product were applied to achieve RDC’s optimum cured thickness of 10-12 dry mils. All dry weather test samples were gently cleaned with dish soap and warm water prior to being photographed.</p>
+                <p class="rdcorange-text bold">This presentation covers several ongoing, practical application performance tests of RDC’s two signature Advanced, High-Performance, Aerospace Grade, Acrylic, Ceramic Coatings. Primers were NEVER applied on any item tested. In most cases, two coats of RDC product were applied to achieve RDC’s optimum cured thickness of 10-12 dry mils. All dry weather test samples were gently cleaned with dish soap and warm water prior to being photographed.</p>
             </div>
         </div>
 
-        <article class="article rdcseablue lightlightgray">
+        <article class="article">
             <div class="row">
                 <div class="col-md-12">
                     <h1>12+ Year (In-Can) Liquid A2 Product Shelf-Life Test  </h1>
@@ -31,14 +31,14 @@ counting.")
 
 
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-5">
                     <h4>Re-opened After 12 Years.</h4>
                     <p>The two images (RIGHT) depict one of six original 1-quart containers of RDC Formula A2 as they appeared when they were reopened in August 2017, with what appears to be some resin (in good condition) floating on the top. This product was originally manufactured and canned in August 2005—12 years prior. These containers had been opened several times over time and were stored indoors under extreme hot and cold environments, with ambient room temperatures ranging from 18°f-160°f for all of the 12 years. </p>
                 </div>
                 <div class="col-md-4">
                     <img src="img/performance/paint.jpg" class="contained-img img-h-300">
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <img src="img/performance/dry_paint.jpg" class="contained-img img-h-300">
                 </div>
             </div>
@@ -51,38 +51,112 @@ counting.")
                 <div class="col-md-4">
                     <img src="img/performance/rehydrated_paint.jpg" class="contained-img img-h-300">
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <img src="img/performance/rehydrated_paint2.jpg" class="contained-img img-h-300">
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-5">
                     <h4>After water added and shaken.</h4>
                     <p>The two images (LEFT) depict (above shown) 12 year old RDC Formula A2 after the product was transferred to a new 1-quart paint can, then a small amount of tap water was added and the can was shaken at a local paint outlet. Amazingly all product remained fully usable after 12 years of storage, all of which was actually used in 2017 to create additional test samples now in progress and shown in this presentation. This is unarguably an AMAZING, unmatched discovery. RDC is not aware of any paint or coating company on this planet that can claim (and prove) a 12+ year shelf-life. </p>
                 </div>
             </div>
         </article>
 
-        <article class="article rdcred lightlightgray">
+    </div>
+    <div class="container-flex rdc rdcsand">
+        <div class="container">
+
+            <article class="article">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>12+ Year Old R33 & R38 Roof Product Sample Tests </h1>
+                        <div class="pillow-20"></div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-5">
+                        <p>Images (RIGHT) depict RDC Roof Formulas R33 & R38 coated bare on two 1/8” thick asphalt roof coating samples in 2005.  Both samples experienced NO DELAMINATION,  NO COATING FAILURE whatsoever, even when bent at 180°—after 12+ years.  This discovery is highly remarkable, especially considering the coating remains like new and is fully pliable, even at a 180° bend, over 12 years after it was applied.</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img src="img/performance/roof_slab1.jpg" class="contained-img img-h-200">
+                    </div>
+                    <div class="col-md-4">
+                        <img src="img/performance/roof_slab1_bend.jpg" class="contained-img img-h-200">
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+    <div class="container">
+
+        <article class="article">
             <div class="row">
                 <div class="col-md-12">
-                    <h1>12+ Year Old R33 & R38 Roof Product Sample Tests </h1>
+                    <h1>Sea-Salt & Tap Water A1/A2 Product Sample Tests </h1>
                     <div class="pillow-20"></div>
                 </div>
             </div>
 
 
             <div class="row">
-                <div class="col-md-4">
-                    <h4>Re-opened After 12 Years.</h4>
-                    <p>Images (RIGHT) depict RDC Roof Formulas R33 & R38 coated bare on two 1/8” thick asphalt roof coating samples in 2005.  Both samples experienced NO DELAMINATION,  NO COATING FAILURE whatsoever, even when bent at 180°—after 12+ years.  This discovery is highly remarkable, especially considering the coating remains like new and is fully pliable, even at a 180° bend, over 12 years after it was applied.</p>
+                <div class="col-md-7">
+                    <p>Images depict over 60 glass jars, half filled with sea-salt water and the other half filled with tap water, with each jar individually containing below listed submersed items, most since Oct 2017.  As of late January 2018, there were no signs of any delamination, nor any evidence of any coating failure:</p>
+                    <ul>
+                        <li>Large Variety of Wood (Bare, Pre-Finished  & Pre-Primed)</li>
+                        <li>Large Variety of Metal (Bare & Pre-Primed)</li>
+                        <li>Large Variety of Alloy Metals, Including Titanium, Aluminum, Galvanized, Stainless & Others </li>
+                        <li>Fiberglass Material (Bare)</li>
+                        <li>Variety of PVC & Plastics</li>
+                        <li>Variety of  Masonry & Concrete Block</li>
+                        <li>Road Asphalt</li>
+                        <li>Vinay Roof Tile </li>
+                        <li>Rubber</li>
+                        <li>Glass</li>
+                    </ul>
                 </div>
-                <div class="col-md-4">
-                    <img src="img/performance/roof_slab1.jpg" class="contained-img img-h-200">
-                </div>
-                <div class="col-md-4">
-                    <img src="img/performance/roof_slab1_bend.jpg" class="contained-img img-h-200">
+                <div class="col-md-3">
+                    <img src="img/performance/jars.jpg" alt="Jars" class="contained-img">
+                    <div class="pillow-20">
+
+                    </div>
+                    <img src="img/performance/jar2.jpg" alt="Jars" class="contained-img">
                 </div>
             </div>
         </article>
-    {{-- container close --}}
+    </div>
+    <div class="container-flex rdc rdcseablue rdcwhite-text">
+        <div class="container">
+
+            <article class="article">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>In-Progress Anti-Corrosion Test With 12-Year-Old RDC A2 Coating </h1>
+                        <div class="pillow-20"></div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="img/performance/lid.jpg" class="contained-img img-h-300">
+                    </div>
+                    <div class="col-md-4">
+                    
+                    </div>
+                    <div class="col-md-4">
+                        <img src="img/performance/lid3.jpg" class="contained-img img-h-300">
+                    </div>
+                    <div class="col-md-12">
+                        <div class="pillow-20">
+
+                        </div>
+                        <p>The three Images (ABOVE) depict the underside of a one-quart paint can lid from one of RDC’s (12- year-old) A2 Formula paint cans (Image 1).  The lid was lightly rinsed off with tap water only, allowed to dry, and then the underside was coated directly over the shown corrosion and rust with RDC’s 12-year-old (revived) A2 Coating.  The lid was then placed in an outdoor weather testing bin mentioned in the previous slide in Aug 2017 (Image 2).  No primers, including rust inhibiting primers were used; only RDC A2 Coating was used for this (and all) tests.  </p>
+
+                        <p>The lid was removed from the bin, was gently cleaned off with soap and water and inspected on Jan 24, 2018.  To date,  there were no signs of any corrosion bleed through, no new corrosion, no delamination, nor any evidence of any coating failure whatsoever, again using 12-year-old RDC A2 Coating.  In fact, the lid looked as good as it did the day it was coated as the images clearly show.  The lid was returned to the bin for continued weather/anti-corrosion testing.</p>
+                    </div>
+                </div>
+            </article>
+        </div>
     </div>
 @endsection
