26e06f16757f93956dcd5349d0e8319eea2a8f93
Peder Tornberg <peder.tornberg@gmail.com>
2018-02-14 21:16:46 +0100

    Reorganized

---
+++ app/Contact/Contact.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    // Table
+    protected $table = 'contact';
+    public $reciever = 'reciever@example.com';
+    public $sender = "sender@example.com";
+    public $sendername = "ContactForm Webpage";
+    public $subject = "ContactForm - Message";
+}
--- app/Http/Controllers/Contact/ContactController.php
+++ app/Http/Controllers/Contact/ContactController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Contact;
 
 use Illuminate\Http\Request;
 use App\Mail\ContactMessage;
