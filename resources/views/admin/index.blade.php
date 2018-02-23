9955b62d8348c5774cdd08b9e251f19d43f8505f
Magnus Andersson <bredsjomagnus@yahoo.se>
2018-02-23 11:22:12 +0000

    Admin layout, adminnavbar, reset password mail via gmail-address

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,15 @@
-@extends('layouts.standard')
+@extends('layouts.admin')
 
 @section('title', 'Admin')
 
 @section('content')
-<h1>Admin</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Admin</h1>
+        </div>
+    </div>
+
+</div>
+
 @endsection
