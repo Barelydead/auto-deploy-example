a14a863cfd5b2507406db3cd29f3f584e97b0d5e
Peder Tornberg <peder.tornberg@gmail.com>
2018-03-03 23:22:55 +0100

    Store contact message in DB

--- app/Contact/Message.php
+++ app/Contact/Message.php
@@ -7,4 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     protected $table = 'contact_messages';
+
+    /**
+     * Add Message to DB
+     *
+     * @param array $data
+     * @param string $reciever
+     * @param string $sender
+     * @param string $subject
+     * @return this
+     */
+    public function addMessage($data, $reciever, $sender, $subject)
+    {
+        $this->reciever = $reciever;
+        $this->sender = $sender;
+        $this->subject = $subject;
+
+        foreach ($data as $key => $val) {
+            $this->{$key} = $val;
+        }
+
+        $this->save();
+        return $this;
+    }
 }
