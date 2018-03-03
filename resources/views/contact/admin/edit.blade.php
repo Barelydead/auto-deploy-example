a9f9c564c58773348e9b2ba964bb127f3a3689b4
Peder Tornberg <peder.tornberg@gmail.com>
2018-03-03 23:30:41 +0100

    Working on contact admin page

--- resources/views/contact/admin/edit.blade.php
+++ resources/views/contact/admin/edit.blade.php
@@ -1,3 +1,12 @@
 @extends('layouts.admin')
 
 @section('title', 'Edit Contact Us')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <h1>Edit Contact Info</h1>
+
+    </div>
+</div>
+@endsection
