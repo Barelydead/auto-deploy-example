94dda2d826576ad40f36ea40284bf75594ecb57f
Magnus Andersson <bredsjomagnus@yahoo.se>
2018-03-08 21:09:06 +0100

    AdminControll edit content imgurl

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -228,7 +228,6 @@ class AdminController extends Controller
         $data['id'] = $request->post('id');
         $data['category'] = $request->post('category');
         $data['title'] = $request->post('title');
-        $data['imgurl'] = $request->post('imgurl');
         $data['content'] = $request->post('content');
 
         /*--------------------------------------------*/
@@ -238,7 +237,6 @@ class AdminController extends Controller
                 ->where('id', $data['id'])
                 ->update([
                     'title'     => $data['title'],
-                    'imgurl'     => $data['imgurl'],
                     'content'   => $data['content']
                 ]);
         }
