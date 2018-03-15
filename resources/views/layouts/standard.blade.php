7cf1becc184a6b5d0a5e5467645c7c561272a858
Christofer Jungberg <kiffen_@hotmail.com>
2018-03-15 14:57:44 +0100

    responsive design for flash area

--- resources/views/layouts/standard.blade.php
+++ resources/views/layouts/standard.blade.php
@@ -4,6 +4,7 @@
         <title>Red Diamond Coatins - @yield('title')</title>
         <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
         {{-- This is the main navbar --}}
