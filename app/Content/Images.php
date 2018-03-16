d3f94d4cb5668af5ddca11609bee389ceba2a9ba
Peder Tornberg <peder.tornberg@gmail.com>
2018-03-16 13:48:16 +0100

    Added content models

---
+++ app/Content/Images.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Content;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Images extends Model
+{
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    protected $table = 'content_images';
+}
