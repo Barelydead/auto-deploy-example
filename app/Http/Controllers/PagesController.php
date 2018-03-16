0cb1d0590f72480e26ac74c03dae68ab3df7e6ae
Peder Tornberg <peder.tornberg@gmail.com>
2018-03-16 13:55:22 +0100

    Working on Content models

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Content;
+use App\Content\Content;
 
 class PagesController extends Controller
 {
