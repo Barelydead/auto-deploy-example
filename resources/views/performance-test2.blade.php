e9842f60c661dcdde5d2c8f2f4c8923670695589
Christofer Jungberg <kiffen_@hotmail.com>
2018-03-19 14:41:17 +0100

    test view that loops articles and images

--- resources/views/performance-test2.blade.php
+++ resources/views/performance-test2.blade.php
@@ -18,14 +18,23 @@ counting.")
             <div class="col-md-12">
                 <h1>{{ $article->title }}</h1>
                 <div class="article-text">
-                    @markdown( $article->content )
-                    @foreach ($images as $img)
-                        @if ($img->content_id == $article->id)
-                            <img src="{{asset("img/upload/" . $img->filename)}}" class="thumbnailimg">
-                        @endif
-                    @endforeach
-                </div>
 
+                    @markdown( $article->content )
+
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="article-thumb-wrap">
+                @foreach ($images as $img)
+                    @if ($img->content_id == $article->id)
+                        <div class="thumb-holder">
+                            <img src="{{asset("img/upload/" . $img->filename)}}" class="img-responsive img-thumbnail">
+                        </div>
+                    @endif
+                @endforeach
+                </div>
             </div>
         </div>
     @endforeach
