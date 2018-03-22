208f851ee8207440ebb20a12ad2610cd4ae2ec1e
Christofer Jungberg <kiffen_@hotmail.com>
2018-03-22 15:20:15 +0100

    fixed absoult paths to asset() in flash

--- resources/views/includes/flash.blade.php
+++ resources/views/includes/flash.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container-fluid" id="flash">
     <div class="row">
-        <div class="flash-image flex-row space-center" style="background-image: url(img/flash/{{$flashImage}})">
+        <div class="flash-image flex-row space-center" style='background-image: url({{asset("img/flash/$flashImage")}})'>
             <div class="flash-text">
                 <h1 class="text-danger">@yield('flash_title')</h1>
                 <p>@yield('flash_text')</p>
