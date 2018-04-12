023269782eae4b256dc422de3a3dc5e861534c3f
Magnus Andersson <bredsjomagnus@yahoo.se>
2018-04-12 12:12:24 +0200

    Conseal login

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -91,8 +91,8 @@
                         </ul>
                     </li>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <!-- <a href="{{ route('login') }}">Login</a> -->
+                        <!-- <a href="{{ route('register') }}">Register</a> -->
                     @endauth
                 </div>
             @endif
