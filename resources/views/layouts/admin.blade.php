26481e6a544170b114238342d29f1d0759a3350e
Magnus Andersson <magnusandersson076@gmail.com>
2018-04-26 14:55:24 +0200

    Add favicon to adminnavbar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,6 +6,7 @@
         <title>Red Diamond Coatings - @yield('title')</title>
         <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+		<link rel="shortcut icon" href="{{{ asset('favicon.ico') }}}">
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
