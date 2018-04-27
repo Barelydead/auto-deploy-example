d91662b195f083c4e3fbf0b7617dca953f207b0f
Magnus Andersson <magnusandersson076@gmail.com>
2018-04-27 10:09:43 +0200

    Meta description to admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+		<meta name="description" content="Welcome to Red Diamond Coatings, Inc. RDC or Red Diamond Coating is a paint that is highly resistant against impact, fire, water and corrosion. RDC products can be applied to virtually any substrate/surface material known to man on Planet Earth.">
         <title>Red Diamond Coatings - @yield('title')</title>
         <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
