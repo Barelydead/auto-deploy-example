22a9882793ee0b4e44fb7031572703842cab892f
Magnus Andersson <magnusandersson076@gmail.com>
2018-04-27 10:35:29 +0200

    Added meta keywords

--- resources/views/layouts/standard.blade.php
+++ resources/views/layouts/standard.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="Welcome to Red Diamond Coatings, Inc. RDC or Red Diamond Coating is a paint that is highly resistant against impact, fire, water and corrosion. RDC products can be applied to virtually any substrate/surface material known to man on Planet Earth.">
-        <title>Red Diamond Coatings - @yield('title')</title>
+		 <meta name="keywords" content="RDC, Red, Diamond, Coating, Coatings, Paint, Materials, Resistant">
+		<title>Red Diamond Coatings - @yield('title')</title>
         <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link rel="shortcut icon" href="{{{ asset('favicon.ico') }}}">
